3a9a5cb73bcf4359f651b9779c4b4732a01b2316
Anderland <pierpaolo.bozzano@gmail.com>
2025-02-04 13:28:54 -0500

    +

--- admin/src/components/MultiSelect/index.tsx
+++ admin/src/components/MultiSelect/index.tsx
@@ -1,9 +1,7 @@
 import React, { useMemo } from 'react';
-import { Field, Flex } from '@strapi/design-system';
-import ReactSelect from './ReactSelect';
+import { Field, Flex, Checkbox } from '@strapi/design-system';
 import { useField } from '@strapi/strapi/admin';
 import { useIntl } from 'react-intl';
-import MultiValueContainer from './MultiValueContainer';
 
 const MultiSelect = ({
   hint,
@@ -15,26 +13,16 @@ const MultiSelect = ({
   description,
   placeholder,
   disabled,
-}: {
-  hint: string;
-  label: string;
-  name: string;
-  intlLabel: any;
-  required: boolean;
-  attribute: any;
-  description: any;
-  placeholder: string;
-  disabled: boolean;
 }) => {
   const { formatMessage } = useIntl();
   const { onChange, value, error } = useField(name);
 
   const possibleOptions = useMemo(() => {
     return (attribute['options'] || [])
-      .map((option: string) => {
-        const [label, value] = [...option.split(/:(.*)/s), option];
-        if (!label || !value) return null;
-        return { label, value };
+      .map((option) => {
+        const [optionLabel, optionValue] = [...option.split(/:(.*)/s), option];
+        if (!optionLabel || !optionValue) return null;
+        return { label: optionLabel, value: optionValue };
       })
       .filter(Boolean);
   }, [attribute]);
@@ -49,7 +37,7 @@ const MultiSelect = ({
     return Array.isArray(parsedValue)
       ? parsedValue
           .map((val) =>
-            possibleOptions.find((option: { label: string; value: string }) => option.value === val)
+            possibleOptions.find((option) => option.value === val)
           )
           .filter((option) => !!option)
       : [];
@@ -59,7 +47,7 @@ const MultiSelect = ({
     if (error) return error;
   
     const { min, max } = attribute;
-    const hasNoOptions = required && (!possibleOptions.length);
+    const hasNoOptions = required && !possibleOptions.length;
     const belowMin = sanitizedValue.length < min && (required || sanitizedValue.length > 0);
     const aboveMax = sanitizedValue.length > max;
   
@@ -78,62 +66,60 @@ const MultiSelect = ({
     return null;
   }, [required, error, possibleOptions, sanitizedValue, attribute]);
 
+  const renderCheckboxes = () => {
+    return possibleOptions.map(({ label: optLabel, value: optValue }) => {
+      const isChecked = sanitizedValue.some((sel) => sel.value === optValue);
+      const maxReached = sanitizedValue.length >= attribute['max'];
+      const disabledOption = disabled || (maxReached && !isChecked);
+
+      const handleCheckboxChange = () => {
+        let newValue;
+        if (isChecked) {
+          newValue = sanitizedValue.filter((sel) => sel.value !== optValue);
+        } else {
+          newValue = [...sanitizedValue, { label: optLabel, value: optValue }];
+        }
+
+        onChange({
+          target: {
+            name,
+            value: newValue.length
+              ? JSON.stringify(newValue.map((v) => v.value))
+              : null,
+            type: attribute.type,
+          },
+        });
+      };
+
+      return (
+        <Checkbox
+          key={optValue}
+          name={name}
+          value={optValue}
+          checked={isChecked}
+          disabled={disabledOption}
+          onChange={handleCheckboxChange}
+        >
+          {formatMessage({ id: optLabel, defaultMessage: optLabel })}
+        </Checkbox>
+      );
+    });
+  };
+
   return (
     <Field.Root
       hint={description?.id ? formatMessage(description) : hint}
-      error={fieldError as string}
+      error={fieldError}
       name={name}
       required={required}
     >
       <Flex direction="column" alignItems="stretch" gap={1}>
-        <Field.Label>{intlLabel?.id ? formatMessage(intlLabel) : label}</Field.Label>
-        <ReactSelect
-          isSearchable={true}
-          isMulti={true}
-          error={fieldError}
-          name={name}
-          id={name}
-          isOptionDisabled={() => sanitizedValue.length >= attribute['max'] || false}
-          isDisabled={disabled || possibleOptions.length === 0}
-          placeholder={placeholder}
-          defaultValue={sanitizedValue.map((val: { label: string; value: string }) => ({
-            label: formatMessage({
-              id: val.label,
-              defaultMessage: val.label,
-            }),
-            value: val.value,
-          }))}
-          components={{
-            MultiValueContainer,
-          }}
-          options={possibleOptions.map((option: { label: string; value: string }) => ({
-            ...option,
-            label: formatMessage({
-              id: option.label,
-              defaultMessage: option.label,
-            }),
-          }))}
-          onChange={(val: any) => {
-            onChange({
-              target: {
-                name: name,
-                value:
-                  val?.length && val.filter((v: any) => !!v)
-                    ? JSON.stringify(val.filter((v: any) => !!v).map((v: any) => v.value))
-                    : null,
-                type: attribute.type,
-              },
-            } as React.ChangeEvent<HTMLInputElement>);
-          }}
-          classNames={{
-            control: (_state: any) => 'select-control',
-            multiValue: (_state: any) => 'select-multi-value',
-            placeholder: (_state: any) => 'select-placeholder',
-            menuList: (_state: any) => 'select-listbox',
-            menu: (_state: any) => 'select-menu',
-            option: (state: any) => (state.isFocused ? 'option-focused' : 'option'),
-          }}
-        />
+        <Field.Label>
+          {intlLabel?.id ? formatMessage(intlLabel) : label}
+        </Field.Label>
+        <Flex direction="column" alignItems="stretch" gap={2}>
+          {renderCheckboxes()}
+        </Flex>
         <Field.Hint />
         <Field.Error />
       </Flex>
